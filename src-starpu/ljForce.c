4af4dc247006531aa2e7eab0003bc1854f4eba92
648trindade <rafaelgt.mbin@gmail.com>
2017-02-07 06:09:46 -0200

    dados particionados e rodando concorrentemente

--- src-starpu/ljForce.c
+++ src-starpu/ljForce.c
@@ -145,8 +145,6 @@ void ljPrint(FILE* file, BasePotential* pot)
 
 int ljForce(SimFlat* s)
 {
-   NWORKERS = starpu_worker_get_count();
-
    LjPotential* pot = (LjPotential *) s->pot;
    real_t sigma = pot->sigma;
    real_t epsilon = pot->epsilon;
@@ -171,67 +169,41 @@ int ljForce(SimFlat* s)
 
    int nNbrBoxes = 27;
 
-   // Handle for s->boxes->nAtoms
-   starpu_data_handle_t nAtoms_handle;
+   // Uma task por box / laço
+   NTASKS = 2;//s->boxes->nLocalBoxes;
+
+   /* Todo dado registrado por um handle poderá(ia) ser usado sem forçar
+      serialização das tasks a partir desse ponto */
+   starpu_data_set_default_sequential_consistency_flag(0);
+
+   // Registra os handles necessários
    starpu_vector_data_register(&nAtoms_handle, STARPU_MAIN_RAM,
       (uintptr_t)s->boxes->nAtoms, s->boxes->nTotalBoxes, sizeof(int));
-
-   // Handle for s->atoms->r
-   starpu_data_handle_t r_handle;
-   starpu_matrix_data_register(&r_handle, STARPU_MAIN_RAM, 
-   (uintptr_t)s->atoms->r, fSize, fSize, 3, sizeof(real_t));
-
-    // Handle for s->atoms->f
-   starpu_data_handle_t f_handle;
-   starpu_matrix_data_register(&f_handle, STARPU_MAIN_RAM, 
-      (uintptr_t)s->atoms->f, fSize, fSize, 3, sizeof(real_t));
-
-    // Handle for s->atoms->U
-   starpu_data_handle_t U_handle;
+   starpu_vector_data_register(&r_handle, STARPU_MAIN_RAM, 
+      (uintptr_t)s->atoms->r, fSize * 3, 
+      sizeof(real_t));
+   starpu_vector_data_register(&f_handle, STARPU_MAIN_RAM, 
+      (uintptr_t)s->atoms->f, s->boxes->nLocalBoxes * MAXATOMS * 3,
+      sizeof(real_t));
    starpu_vector_data_register(&U_handle, STARPU_MAIN_RAM, 
-      (uintptr_t)s->atoms->U, fSize, sizeof(real_t));
-
-    // Handle for ePot
-   starpu_data_handle_t ePot_handle;
+      (uintptr_t)s->atoms->U, s->boxes->nLocalBoxes * MAXATOMS, sizeof(real_t));
    starpu_variable_data_register(&ePot_handle, STARPU_MAIN_RAM, 
       (uintptr_t)&ePot, sizeof(real_t));
+   starpu_vector_data_register(&nbrBoxes_handle, STARPU_MAIN_RAM, 
+      (uintptr_t)s->boxes->nbrBoxes[0], s->boxes->nLocalBoxes * 27, 
+      sizeof(s->boxes->nbrBoxes[0][0]));
    
-   // Handle for s->boxes->nbrBoxes
-    starpu_data_handle_t boxes_handle;
-    starpu_vector_data_register(&boxes_handle, STARPU_MAIN_RAM, 
-       (uintptr_t)s->boxes->nbrBoxes[0], s->boxes->nLocalBoxes * 27, 
-       sizeof(s->boxes->nbrBoxes[0][0]));
+   // Dados serão particionados pelo número de tasks
+   data_filter.nchildren = NTASKS;
+   starpu_data_partition(nbrBoxes_handle, &data_filter);
+   starpu_data_partition(U_handle, &data_filter);
+   starpu_data_partition(f_handle, &data_filter);
 
-   struct starpu_data_filter f = {
-      .filter_func = starpu_vector_filter_block,
-      .nchildren = NWORKERS
-   };
-
-   starpu_data_partition(boxes_handle, &f);
-
-   struct starpu_codelet ePot_redux_codelet = {
-      .cpu_funcs = {ePot_redux_cpu_func},
-      .cpu_funcs_name = {"ePot_redux_cpu_func"},
-      .modes = {STARPU_RW, STARPU_R},
-      .nbuffers = 2,
-      .name = "redux"
-   };
-
-   struct starpu_codelet ePot_init_codelet =
-   {
-      .cpu_funcs = {ePot_init_cpu_func},
-      .cpu_funcs_name = {"ePot_init_cpu_func"},
-      .modes = {STARPU_W},
-      .nbuffers = 1,
-      .name = "init"
-   };
-
-   /* Set the methods to define neutral elements and to perform the reduction operation */
+   // Dita as funções que definem inicialização de instâncias e redução de ePot
    starpu_data_set_reduction_methods(ePot_handle, &ePot_redux_codelet, &ePot_init_codelet);
 
-   struct params* params_created = comdMalloc(sizeof(struct params) * NWORKERS);
-   // copia paramêtros
-   struct params params_base = {
+   // Copia paramêtros
+   struct params params = {
       .s6 = s6, 
       .eShift = eShift,
       .epsilon = epsilon, 
@@ -240,58 +212,43 @@ int ljForce(SimFlat* s)
       .nLocalBoxes = s->boxes->nLocalBoxes
    };
 
-   // codelet definition
-   struct starpu_codelet cl = {
-      .cpu_funcs = { cpu_func },
-      .cpu_funcs_name = { "cpu_func" },
-#ifdef STARPU_USE_CUDA
-	   .cuda_funcs = { gpu_func },
-      .cuda_flags = {STARPU_CUDA_ASYNC},
-#endif
-      .nbuffers = 6,
-      .modes = {STARPU_R, STARPU_R, STARPU_R, STARPU_RW, STARPU_RW, STARPU_REDUX}
-      //        boxes,    nAtoms,   atoms->r, atoms->f,  atoms->U,  ePot
-   };
-
    // loop over local boxes
    //#pragma omp parallel for reduction(+:ePot)
-   for (int id = 0; id < NWORKERS; id++)
+   for (int id = 0; id < NTASKS; id++)
    {
-      // coração do laço retirado e movido pra cpu_func()
-      struct params *params = &params_created[id];
-      *params = params_base;
-      params->id = id;
-
-      // task definition
+      // Coração do laço retirado e movido pra cpu_func()
+      // Definição da task
       struct starpu_task *task = starpu_task_create();
       task->cl = &cl;
-      task->cl_arg = params;
+      task->cl_arg = &params;
       task->cl_arg_size = sizeof(struct params);
-      //task->synchronous = 1;
-      // assign handles to task
-      task->handles[0] = starpu_data_get_sub_data(boxes_handle, 1, id);
+      task->synchronous = 0;
+      // Atribui handles para a task
+      task->handles[0] = starpu_data_get_sub_data(nbrBoxes_handle, 1, id);
       task->handles[1] = nAtoms_handle;
       task->handles[2] = r_handle;
-      task->handles[3] = f_handle;
-      task->handles[4] = U_handle;
+      task->handles[3] = starpu_data_get_sub_data(f_handle, 1, id);
+      task->handles[4] = starpu_data_get_sub_data(U_handle, 1, id);
       task->handles[5] = ePot_handle;
 
-      // submit task
+      // Submete a task assincronamente
+      // FIXME: workers trabalhando serialmente
       int ret = starpu_task_submit(task);
       STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_submit");
    } // loop over local boxes in system
 
    starpu_task_wait_for_all();
-   starpu_data_unpartition(boxes_handle, STARPU_MAIN_RAM);
+   starpu_data_unpartition(nbrBoxes_handle, STARPU_MAIN_RAM);
+   starpu_data_unpartition(U_handle, STARPU_MAIN_RAM);
+   starpu_data_unpartition(f_handle, STARPU_MAIN_RAM);
 
-   // Unregister all data handles and free memory
+   // Desregistra todos os handles
    starpu_data_unregister(nAtoms_handle);
    starpu_data_unregister(r_handle);
    starpu_data_unregister(f_handle);
    starpu_data_unregister(U_handle);
    starpu_data_unregister(ePot_handle);
-   starpu_data_unregister(boxes_handle);
-   comdFree(params_created);
+   starpu_data_unregister(nbrBoxes_handle);
 
    ePot = ePot*4.0*epsilon;
    s->ePotential = ePot;
@@ -301,39 +258,53 @@ int ljForce(SimFlat* s)
    return 0;
 }
 
-// ljForce main loop (iterate over all boxes)
+// Laço principal do ljForce (itera sobre todas as caixas) 
 void cpu_func(void *buffers[], void *cl_arg){
     
-    // gathering paramethers
+    // Angariando parâmetros
     struct params *params = cl_arg;
     real_t       s6 = params->s6;
     real_t   eShift = params->eShift;
     real_t  epsilon = params->epsilon;
     real_t    rCut2 = params->rCut2;
-    int          id = params->id;
     int   nNbrBoxes = params->nNbrBoxes;
     int nLocalBoxes = params->nLocalBoxes;
+    
+    // Angariando buffers
+    int* nbrBoxes = (   int*) STARPU_VECTOR_GET_PTR(buffers[0]);
+    int*   nAtoms = (   int*) STARPU_VECTOR_GET_PTR(buffers[1]);
+    real3*      r = ( real3*) STARPU_VECTOR_GET_PTR(buffers[2]);
+    real3*      f = ( real3*) STARPU_VECTOR_GET_PTR(buffers[3]);
+    real_t*     U = (real_t*) STARPU_VECTOR_GET_PTR(buffers[4]);
+    real_t*  ePot = (real_t*) STARPU_VARIABLE_GET_PTR(buffers[5]);
+    
+    // Angariando offsets e números de elementos
+    size_t nbrBoxes_offset = (size_t) STARPU_VECTOR_GET_OFFSET(buffers[0]);
+    size_t nbrBoxes_nx = (size_t) STARPU_VECTOR_GET_NX(buffers[0]);
+    size_t iOff_offset = (size_t) STARPU_VECTOR_GET_OFFSET(buffers[4]);
+    size_t f_nx = (size_t) STARPU_VECTOR_GET_NX(buffers[3]);
+    size_t U_nx = (size_t) STARPU_VECTOR_GET_NX(buffers[4]);
 
-    //printf("%d\n", starpu_worker_get_id());
+    // Conferindo se offsets e tamanhos estão dentro do esperado
+    STARPU_ASSERT((nbrBoxes_offset / sizeof(int)) % nNbrBoxes == 0);
+    STARPU_ASSERT(nbrBoxes_nx % nNbrBoxes == 0);
+    STARPU_ASSERT((iOff_offset / sizeof(real_t)) % MAXATOMS == 0);
+    STARPU_ASSERT(U_nx % MAXATOMS == 0);
+    STARPU_ASSERT((f_nx / 3) % MAXATOMS == 0);
+    
+    // Calculando offsets e tamanhos reais
+    nbrBoxes_offset /= nNbrBoxes * sizeof(int);
+    nbrBoxes_nx     /= nNbrBoxes;
+    iOff_offset     /= sizeof(real_t);
 
-    // gathering buffers
-    int*   boxes = (   int*) STARPU_VECTOR_GET_PTR(buffers[0]);
-    int*  nAtoms = (   int*) STARPU_VECTOR_GET_PTR(buffers[1]);
-    real3*     r = ( real3*) STARPU_VECTOR_GET_PTR(buffers[2]);
-    real3*     f = ( real3*) STARPU_VECTOR_GET_PTR(buffers[3]);
-    real_t*    U = (real_t*) STARPU_VECTOR_GET_PTR(buffers[4]);
-    real_t* ePot = (real_t*) STARPU_VARIABLE_GET_PTR(buffers[5]);
-
-    int slices = (nLocalBoxes + NWORKERS - 1) / NWORKERS;
-
-    for (int iBox = id * slices; iBox < (id + 1) * slices && iBox < nLocalBoxes; iBox++){
+    for (int iBox = nbrBoxes_offset; iBox < nbrBoxes_offset + nbrBoxes_nx && iBox < nLocalBoxes; iBox++){
 
       int nIBox = nAtoms[iBox];
       
       // loop over neighbors of iBox
       for (int jTmp=0; jTmp<nNbrBoxes; jTmp++){
-            // Access boxes in a array-like way (contiguous memory)
-            int jBox = boxes[(iBox - id * slices) * nNbrBoxes + jTmp];
+            // Acessa nbrBoxes de modo contíguo
+            int jBox = nbrBoxes[(iBox - nbrBoxes_offset) * nNbrBoxes + jTmp];
             
             assert(jBox>=0);
             
@@ -341,6 +312,8 @@ void cpu_func(void *buffers[], void *cl_arg){
             
             // loop over atoms in iBox
             for (int iOff=MAXATOMS*iBox; iOff<(iBox*MAXATOMS+nIBox); iOff++){
+               // iOff traduzido para uso em dados particionados
+               int task_iOff = iOff - iOff_offset;
 
                // loop over atoms in jBox
                for (int jOff=jBox*MAXATOMS; jOff<(jBox*MAXATOMS+nJBox); jOff++){
@@ -358,14 +331,13 @@ void cpu_func(void *buffers[], void *cl_arg){
                      r2 = 1.0/r2;
                      real_t r6 = s6 * (r2*r2*r2);
                      real_t eLocal = r6 * (r6 - 1.0) - eShift;
-                     U[iOff] += 0.5*eLocal;
-                     // zona critica
-                     *ePot += 0.5*eLocal;
+                     U[task_iOff] += 0.5*eLocal;
+                     *ePot = *ePot + 0.5*eLocal;
 
                      // different formulation to avoid sqrt computation
                      real_t fr = - 4.0*epsilon*r6*r2*(12.0*r6 - 6.0);
                      for (int m=0; m<3; m++)
-                           f[iOff][m] -= dr[m]*fr;
+                           f[task_iOff][m] -= dr[m]*fr;
                   }
                } // loop over atoms in jBox
          } // loop over atoms in iBox
@@ -378,7 +350,7 @@ void ePot_redux_cpu_func(void *descr[], void *cl_arg)
 	real_t *ePot = (real_t *)STARPU_VARIABLE_GET_PTR(descr[0]);
 	real_t *ePot_partial = (real_t *)STARPU_VARIABLE_GET_PTR(descr[1]);
 
-	*ePot += *ePot_partial;
+      *ePot = *ePot + *ePot_partial;
 }
 
 void ePot_init_cpu_func(void *descr[], void *cl_arg)
--- src-starpu/starpu_code.h
+++ src-starpu/starpu_code.h
@@ -1,12 +1,23 @@
 #include <starpu.h>
 #include "mytype.h"
 
-static int NWORKERS = 1;
-
 // defined in implemented in starpu_code.c
 struct params {
     real_t s6, eShift, epsilon, rCut2;
-    int id, nNbrBoxes, nLocalBoxes;
+    int nNbrBoxes, nLocalBoxes;
+};
+
+static starpu_data_handle_t nAtoms_handle;
+static starpu_data_handle_t r_handle;
+static starpu_data_handle_t f_handle;
+static starpu_data_handle_t U_handle;
+static starpu_data_handle_t ePot_handle;
+static starpu_data_handle_t nbrBoxes_handle;
+static int NTASKS = 1;
+
+static struct starpu_data_filter data_filter = {
+    .filter_func = starpu_vector_filter_block,
+    .nchildren = 1 // alterado antes da criação das tasks
 };
 
 void cpu_func(void *buffers[], void *cl_arg);
@@ -15,3 +26,32 @@ void gpu_func(void *buffers[], void *cl_arg);
 #endif
 void ePot_redux_cpu_func(void *descr[], void *cl_arg);
 void ePot_init_cpu_func(void *descr[], void *cl_arg);
+
+static struct starpu_codelet ePot_redux_codelet = {
+    .cpu_funcs = {ePot_redux_cpu_func},
+    .cpu_funcs_name = {"ePot_redux_cpu_func"},
+    .modes = {STARPU_RW, STARPU_R},
+    .nbuffers = 2,
+    .name = "redux"
+};
+
+static struct starpu_codelet ePot_init_codelet = {
+    .cpu_funcs = {ePot_init_cpu_func},
+    .cpu_funcs_name = {"ePot_init_cpu_func"},
+    .modes = {STARPU_W},
+    .nbuffers = 1,
+    .name = "init"
+};
+
+// codelet definition
+static struct starpu_codelet cl = {
+    .cpu_funcs = { cpu_func },
+    .cpu_funcs_name = { "cpu_func" },
+#ifdef STARPU_USE_CUDA
+    .cuda_funcs = { gpu_func },
+    .cuda_flags = {STARPU_CUDA_ASYNC},
+#endif
+    .nbuffers = 6,
+    .modes = {STARPU_R, STARPU_R, STARPU_R, STARPU_RW, STARPU_RW, STARPU_REDUX}
+    //        boxes,    nAtoms,   atoms->r, atoms->f,  atoms->U,  ePot
+};
