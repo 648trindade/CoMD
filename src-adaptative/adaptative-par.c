07e712755e02bec51a112c92b7ee2ab7a1d5b04f
Rafael Gauna Trindade <rtrindade@inf.ufsm.br>
2019-04-02 11:33:50 -0300

    updates adaptativo

--- src-adaptative/adaptative-par.c
+++ src-adaptative/adaptative-par.c
@@ -14,6 +14,7 @@ typedef struct{
     size_t f; // Início do intervalo
     size_t l; // Fim do intervalo
     size_t m; // Tamanho do intervalo
+    size_t c; // Tamanho do chunk
 } adpt_range_t;
 
 typedef struct {
@@ -33,6 +34,7 @@ void initialize_worker_data(worker_data_t* wrkr_d, size_t thr_id, size_t nthr, s
     wrkr_d->range.f = chunk * thr_id + first + MIN(thr_id, remain); // Ponto inicial do intervalo
     wrkr_d->range.l = wrkr_d->range.f + chunk + (size_t)(thr_id < remain); // Ponto final
     wrkr_d->range.m = wrkr_d->range.l - wrkr_d->range.f;  // Tamanho original do intervalo
+    wrkr_d->range.c = LOG2(wrkr_d->range.m); // Tamanho do chunk a ser extraído serialmente
 
     omp_init_lock(&(wrkr_d->lock)); // Inicializa a trava
 }
@@ -47,19 +49,32 @@ void destroy_worker_data(worker_data_t* wrkr_d){
  * --------------------------
  *   Extrai trabalho sequencial do intervalo pertencente a thread corrente.
  *   A quantia de trabalho a ser extraída é ALPHA * log2(m), onde m é o tamanho do intervalo.
+ *   Caso detecte um conflito (algum ladrão rouba parte do trabalho que ele 
+ *   extraíria), tenta extrair novamente.
  * 
- *   m_range : ponteiro para o intervalo da thread corrente
- *         f : o índice inicial do trabalho extraído
- *         l : o índice final do trabalho extraído
+ *   wrkr_d : estrutura com info. do worker
+ *        f : o índice inicial do trabalho extraído
+ *        l : o índice final do trabalho extraído
  * 
  *   returns : o tamanho do intervalo extraído
  */
 size_t adpt_extract_seq(worker_data_t* wrkr_d, size_t* f, size_t* l){
-    *f = wrkr_d->range.f;
-    *l = MIN(wrkr_d->range.l, *f + (size_t)(ALPHA * LOG2(wrkr_d->range.m)));
-    wrkr_d->range.f = *l;
-    // fprintf(stderr, "%.6lf [Worker %d] adpt_extract_seq %d a %d\n", omp_get_wtime(), omp_get_thread_num(), *f, *l);
-    return *l - *f;
+    while(1) {
+        *f = wrkr_d->range.f;
+        *l = MIN(wrkr_d->range.l, *f + (size_t)(ALPHA * wrkr_d->range.c));
+        wrkr_d->range.f = *l;
+        if (wrkr_d->range.f > wrkr_d->range.l) { // Conflito: desfaz e tenta de novo
+            wrkr_d->range.f = *f;
+#ifdef ADPT_DEBUG
+            fprintf(stderr, "%.6lf [Worker %d] conflitou ao extrair trabalho\n", omp_get_wtime(), wrkr_d->id);
+#endif
+            continue;
+        }
+#ifdef ADPT_DEBUG
+        fprintf(stderr, "%.6lf [Worker %d] adpt_extract_seq %d a %d (%d)\n", omp_get_wtime(), omp_get_thread_num(), *f, *l, wrkr_d->range.l);
+#endif
+        return *l - *f;
+    }
 }
 
 /*
@@ -73,12 +88,9 @@ size_t adpt_extract_seq(worker_data_t* wrkr_d, size_t* f, size_t* l){
  *   A quantia de trabalho necessária é um valor entre ((m - m')/2) e (sqrt(m)),
  *   onde m é o tamanho do intervalo e m' é o tamanho do intervalo já trabalhado.
  * 
- *      ranges : vetor de intervalos
- *     m_range : intervalo da thread corrente
- *       locks : vetor de travas
- *   m_visited : vetor auxiliar que armazena os intervalos que já foram inspecionados
- *        nthr : número total de threads da aplicação
- *      thr_id : ID referente a thread corrente
+ *    theft_d : estrutura com info. do ladrão
+ *   v_wrkr_d : vetor de estruturas com info. dos workers
+ *       nthr : número total de threads da aplicação
  * 
  *   returns : 1 se o roubo for bem sucedido, 0 do contrário
  */
@@ -87,33 +99,44 @@ size_t adpt_extract_par(
 ){
     size_t success     = 0;
     size_t remaining   = nthr - 1;
+    size_t i;
 
     memset(theft_d->visited, 0, nthr);
     theft_d->visited[theft_d->id] = 1;
 
     // Enquanto houver intervalos que não foram inspecionadas
     while((remaining > 0) && !success){
-        size_t i = rand() % nthr; // Sorteia uma intervalo
-        while (theft_d->visited[i] != 0) i = (i+1) % nthr; // Avança até um intervalo ainda não visto
+        i = rand() % nthr; // Sorteia uma intervalo
+        for (; theft_d->visited[i]; i = (i+1)%nthr); // Avança até um intervalo ainda não visto
         worker_data_t* victim_d = &(v_wrkr_d[i]);
-        // Testa se o intervalo não está travado e trava se estiver livre.
-        // omp_test_lock é uma chamada não-bloqueante
-        if (omp_test_lock(&(victim_d->lock))){
-            size_t m_left = victim_d->range.l - victim_d->range.f;  // Calcula o trabalho restante
-            size_t min    = sqrt(victim_d->range.m);          // Calcula o tamanho mínimo para roubo
-            size_t steal_size  = MAX(m_left >> 1, min); // Calcula o tamanho do roubo
 
-            // Testa se o restante é maior ou igual ao mínimo a ser roubado
-            // FIX: não roubar se falta só um.
-            if ((m_left >= min) && (steal_size > 1)){
-                // Efetua o roubo, atualizando os intervalos
-                theft_d->range.f  = victim_d->range.l - steal_size;
-                theft_d->range.l  = victim_d->range.l;
-                victim_d->range.l = theft_d->range.f;
-                theft_d->range.m  = theft_d->range.l - theft_d->range.f;
-                
-                // fprintf(stderr, "%.6lf [Worker %d] adpt_extract_par %d a %d\n", omp_get_wtime(), thr_id, m_range->f, m_range->l);
-                success = 1;
+        // Testa se o intervalo não está travado e trava se estiver livre.
+        if (omp_test_lock(&(victim_d->lock))){
+            size_t vic_l = victim_d->range.l, vic_f = victim_d->range.f; // cópias
+            size_t m_left = vic_l - vic_f; // Trabalho restante
+            size_t min = (size_t)sqrt(victim_d->range.m); // Tamanho mínimo para roubo
+            size_t steal_size = MAX(m_left >> 1, min); // Tamanho do roubo
+            size_t begin = vic_l - steal_size;
+
+            // O restante é maior ou igual ao mínimo a ser roubado?
+            if ((m_left > min) && (steal_size > 1)){
+                victim_d->range.l = begin;
+                if (victim_d->range.f < begin){ // Inicio a frente do inicio da vitima
+                    // ativa a própria trava pra ninguém roubar dele enquanto ele rouba
+                    omp_set_lock(&(theft_d->lock));
+                    // Efetua o roubo, atualizando os intervalos
+                    theft_d->range.f  = begin;
+                    theft_d->range.l  = vic_l;
+                    theft_d->range.m  = steal_size;
+                    omp_unset_lock(&(theft_d->lock));
+                    success = 1;
+                }
+                else{ // Conflito: desfaz e aborta
+                    victim_d->range.l = vic_l;
+#ifdef ADPT_DEBUG
+                    fprintf(stderr, "%.6lf [Worker %d] conflitou ao roubar de %lu\n", omp_get_wtime(), theft_d->id, i);
+#endif
+                }
             }
             else { // Caso não houver trabalho suficiente
                 theft_d->visited[i] = 1; // Marca como visitado
@@ -122,8 +145,12 @@ size_t adpt_extract_par(
             omp_unset_lock(&(victim_d->lock)); // Destrava o intervalo
         }
     }
-    // if (!success)
-    //     fprintf(stderr, "%.6lf [Worker %d] sem trabalho disponivel\n", omp_get_wtime(), thr_id);
+#ifdef ADPT_DEBUG
+    if (success)
+        fprintf(stderr, "%.6lf [Worker %d] adpt_extract_par %d a %d de %lu\n", omp_get_wtime(), theft_d->id, theft_d->range.f, theft_d->range.l, i);
+    else
+        fprintf(stderr, "%.6lf [Worker %d] sem trabalho disponivel\n", omp_get_wtime(), theft_d->id);
+#endif
     return success;
 }
 
@@ -184,8 +211,8 @@ double adpt_parallel_for_and_reduce(
 
         while(1){ // Itera enquanto houver trabalho a ser feito
             // Itera enquanto houver trabalho sequencial a ser feito
-            while (adpt_extract_seq(m_worker_data, &first, &last) > 0)
-                for (size_t i = first; i < last; i++)
+            while (adpt_extract_seq(m_worker_data, &m_first, &m_last) > 0)
+                for (size_t i = m_first; i < m_last; i++)
                     // Chama o kernel da aplicação e aplica redução no resultado retornado
                     m_rdct_var = rdct_fun(m_rdct_var, kernel(data, i));
             
@@ -219,6 +246,9 @@ void adpt_parallel_for(
     size_t nthr = omp_get_max_threads(); // número de threads
     worker_data_t* workers_data = malloc(nthr * sizeof(worker_data_t)); // Dados para os workers
 
+#ifdef ADPT_DEBUG
+    fprintf(stderr, "%.6lf ============ STARTING ==============\n", omp_get_wtime());
+#endif
     #pragma omp parallel shared(kernel, data, workers_data, nthr, first, last)
     {
         size_t m_first = first, m_last = last; // cópia dos limites originais
--- src-adaptative/adaptative-par.h
+++ src-adaptative/adaptative-par.h
@@ -1,13 +1,15 @@
 #ifndef ADAPTATIVEPAR_H_
 #define ADAPTATIVEPAR_H_
 
+#include <stddef.h>
+
 double adpt_parallel_for_and_reduce(
-    double (*kernel)(void*, size_t, size_t), void* data, size_t first, size_t last, 
+    double (*kernel)(void*, size_t), void* data, size_t first, size_t last, 
     double start_value, double (*rdct_fun)(double, double)
 );
 
-void adapt_parallel_for(
-    void (*kernel)(void*, size_t, size_t), void* data, size_t first, size_t last
+void adpt_parallel_for(
+    void (*kernel)(void*, size_t), void* data, size_t first, size_t last
 );
 
 #endif //ADAPTATIVEPAR_H_
